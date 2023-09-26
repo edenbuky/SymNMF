4af57743927274321d20bdac3d1294048f215bcd
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-09-26 19:58:18 +0300

    Update symnmf.c

--- symnmf.c
+++ symnmf.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include "symnmf.h"
 
 #define CHECK_MEMORY_ALLOCATION(ptr) \
     do { \
@@ -56,6 +57,46 @@ double** sym(point* points, int n, int d) {
     return A;
 }
 
+double** ddg(double** A, int n) {
+    // Allocate memory for the diagonal degree matrix D
+    double** D = (double**)malloc(n * sizeof(double*));
+    CHECK_MEMORY_ALLOCATION(D);
+    for (int i = 0; i < n; i++) {
+        D[i] = (double*)calloc(n, sizeof(double)); // Initialize with zeros
+        CHECK_MEMORY_ALLOCATION(D[i]);
+    }
+
+    // Calculate the diagonal degree matrix D
+    for (int i = 0; i < n; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < n; j++) {
+            sum += A[i][j];
+        }
+        D[i][i] = sum;  // Set the diagonal element
+    }
+
+    return D;
+}
+
+double** norm(double** A, double** D, int n) {
+    // Allocate memory for the normalized similarity matrix W
+    double** W = (double**)malloc(n * sizeof(double*));
+    CHECK_MEMORY_ALLOCATION(W);
+    for (int i = 0; i < n; i++) {
+        W[i] = (double*)malloc(n * sizeof(double));
+        CHECK_MEMORY_ALLOCATION(W[i]);
+    }
+
+    // Calculate the normalized similarity matrix W
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            W[i][j] = A[i][j] / (sqrt(D[i][i]) * sqrt(D[j][j]));
+        }
+    }
+
+    return W;
+}
+
 
 void printMatrix(double** matrix, int n) {
     for (int i = 0; i < n; i++) {
@@ -64,5 +105,79 @@ void printMatrix(double** matrix, int n) {
         }
         printf("\n");
     }
+}
+
+point* readPointsFromFile(const char* filename, int* numPoints, int* dimensions) {
+    char line[256];
+    FILE* file;
+    int count, pointIndex, coordinateIndex;
+    char* token;
+    point* points;
+    *numPoints = 0;
+    *dimensions = 0;
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printInvalidInputError("An Error Has Occurred\n");
+        return NULL;
+    }
+    while (fgets(line, sizeof(line), file)) {
+        (*numPoints)++;
+        count = 0;
+        token = strtok(line, ",");
+        while (token != NULL) {
+            count++;
+            token = strtok(NULL, ",");
+        }
+        if (*dimensions == 0) {
+            *dimensions = count;
+        } else if (*dimensions != count) {
+            fclose(file);
+            printInvalidInputError("An Error Has Occurred\n");
+            return NULL;
+        }
+    }
+    
+    points = (point*)malloc(*numPoints * sizeof(point));
+    CHECK_MEMORY_ALLOCATION(points);
+
+    rewind(file);
+
+    pointIndex = 0;
+    while (fgets(line, sizeof(line), file)) {
+        points[pointIndex].coordinates = (double*)malloc(*dimensions * sizeof(double));
+        if (points[pointIndex].coordinates == NULL) {
+            freePoints(points, pointIndex);
+            printInvalidInputError("An Error Has Occurred\n");
+            return NULL;
+        }
+
+        coordinateIndex = 0;
+        token = strtok(line, ",");
+        while (token != NULL) {
+            points[pointIndex].coordinates[coordinateIndex] = atof(token);
+            coordinateIndex++;
+            token = strtok(NULL, ",");
+        }
+        ++pointIndex;
+    }
+    fclose(file);
+    return points;
+}
+void freePoints(point* points, int numPoints) {
+    int i;
+    if (points == NULL) {
+        return;
+    }
+    for (i = 0; i < numPoints; i++) {
+        if (points[i].coordinates != NULL){
+            free(points[i].coordinates);
+        }
+    }
+
+    free(points);
+}
+void printInvalidInputError(const char* message) {
+    printf("%s\n", message);
+    exit(1);
 }
 
