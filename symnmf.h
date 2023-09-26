3a5bd440cacbaa547de8fc812298e09fd1818989
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-09-26 19:58:55 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -1,16 +1,24 @@
-#define CHECK_MEMORY_ALLOCATION(ptr) \
-    do { \
-        if (!(ptr)) { \
-            fprintf(stderr, "An error has occurred: Memory allocation failed.\n"); \
-            exit(EXIT_FAILURE); \
-        } \
-    } while (0)
-
-
+#ifndef symnmf.h
+#define symnmf.h
+double euclideanDistance(double* p, double* q, int d);
+double** sym(point* points, int n, int d);
+double** ddg(double** A, int n);
+double** norm(double** A, double** D, int n);
+static PyObject *fit(PyObject *self, PyObject *args);
 typedef struct {
     double *coordinates;
 } point;
-
-double** sym(point* points, int n, int d);
-double euclideanDistance(double* p, double* q, int d);
-void printMatrix(double** matrix, int n);
+#endif //symnmf.h
+#define CHECK_DOUBLE_POINTER_MEMORY_ALLOCATION(ptr, size) \
+    do { \
+        if (!(ptr)) { \
+            fprintf(stderr, "An error has occurred: Memory allocation for double pointer failed.\n"); \
+            exit(EXIT_FAILURE); \
+        } \
+        for (int i = 0; i < (size); i++) { \
+            if (!(ptr)[i]) { \
+                fprintf(stderr, "An error has occurred: Memory allocation for inner pointer failed.\n"); \
+                exit(EXIT_FAILURE); \
+            } \
+        } \
+    } while (0)
