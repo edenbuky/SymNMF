8c5b82547754ebd0a415d3474b126eabcb75502e
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-09-28 18:51:41 +0300

    Update symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -1,24 +1,18 @@
-#ifndef symnmf.h
-#define symnmf.h
+#ifndef symnmf_h
+#define symnmf_h
+
+typedef struct {
+    double *coordinates;
+}point;
+
+point* readPointsFromFile(const char* filename, int* numPoints, int* dimensions);
 double euclideanDistance(double* p, double* q, int d);
 double** sym(point* points, int n, int d);
 double** ddg(double** A, int n);
 double** norm(double** A, double** D, int n);
-static PyObject *fit(PyObject *self, PyObject *args);
-typedef struct {
-    double *coordinates;
-} point;
-#endif //symnmf.h
-#define CHECK_DOUBLE_POINTER_MEMORY_ALLOCATION(ptr, size) \
-    do { \
-        if (!(ptr)) { \
-            fprintf(stderr, "An error has occurred: Memory allocation for double pointer failed.\n"); \
-            exit(EXIT_FAILURE); \
-        } \
-        for (int i = 0; i < (size); i++) { \
-            if (!(ptr)[i]) { \
-                fprintf(stderr, "An error has occurred: Memory allocation for inner pointer failed.\n"); \
-                exit(EXIT_FAILURE); \
-            } \
-        } \
-    } while (0)
+void printMatrix(double** matrix, int n);
+void printInvalidInputError(const char* message);
+void freePoints(point* points, int numPoints);
+//static PyObject *fit(PyObject *self, PyObject *args);
+
+#endif
