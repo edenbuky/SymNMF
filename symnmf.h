781dec567904deec8ef3dfa44ec25778cacf6707
Eden Buky <92543097+edenbuky@users.noreply.github.com>
2023-10-01 13:05:27 +0300

    add Yeal functions symnmf.h

--- symnmf.h
+++ symnmf.h
@@ -4,7 +4,12 @@
 typedef struct {
     double *coordinates;
 }point;
-
+struct matrix{
+    int r;
+    int c;
+    double ** data;
+};
+typedef struct matrix matrix;
 point* readPointsFromFile(const char* filename, int* numPoints, int* dimensions);
 double euclideanDistance(double* p, double* q, int d);
 double** sym(point* points, int n, int d);
@@ -15,5 +20,17 @@ void printInvalidInputError(const char* message);
 void freePoints(point* points, int numPoints);
 void freeMatrix(double** matrix, int numPoints);
 double squaredFrobeniusNorm(double** matrix, int m, int n);
+matrix * updateH(matrix *H, matrix *W. int iter, double epsilon);
+matrix* oneIter(matrix * H, matrix * W);
+matrix* transpose(matrix* m);
+void diagPow(matrix * m);
+matrix* matMul(matrix* m1, matrix* m2);
+void printMat(matrix * m);
+double * getCol(matrix* mat, int index);
+double mulSum(double* arr1, double* arr2, int l);
+matrix * create_matrix(double ** arr, int r, int c);
+void set(matrix* mat, int i, int j, double val);
+double get(matrix* mat, int i, int j);
+
 
 #endif
